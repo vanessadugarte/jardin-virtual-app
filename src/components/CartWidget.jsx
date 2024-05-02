7b4440f8e346fd9cd9abc7dfc776fa548875893d
Vanessa Dugarte <dugartevanessa@gmail.com>
2024-05-02 09:19:41 -0400

    create components files

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartWidget = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CartWidget;
---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const CategoryList = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="#">Plantas</a>
+                </li>
+                <li>
+                    <a href="#">Maceteros</a>
+                </li>
+                <li>
+                    <a href="#">Accesorios</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default CategoryList;
