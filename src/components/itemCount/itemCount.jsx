e3c7e28a1822135e9c9f07f9edd9d2c4c99c6ae5
Vanessa Dugarte <dugartevanessa@gmail.com>
2024-05-02 20:24:17 -0400

    add decrement function

--- src/components/itemCount/itemCount.jsx
+++ src/components/itemCount/itemCount.jsx
@@ -5,7 +5,7 @@ export const ItemCount = ({stock, count, increment, decrement}) => {
     return (
         <div className="itemCountContainer">
             <div className="itemCountCounterContainer">
-                <button className="itemCountButton" onClick={decrement}>
+                <button className="itemCountButton" onClick={decrement} disabled={count <= 0}>
                     -
                 </button>
                 <span className="itemCountButton">{count}</span>
