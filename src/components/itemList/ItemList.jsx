fc32f064dc805c202a39a0716d19ffed071284ec
Vanessa Dugarte <dugartevanessa@gmail.com>
2024-05-02 22:05:40 -0400

    ItemList fixed

--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./item.css"
 
-export const ItemList = ({products}) {
+const ItemList = ({products}) => {
     return (
         <div className="itemListContainer">
             {products.map((product) =>(
                 <Item key={product.id} item={product}/>
-                ))}
+            ))}
         </div>
     );
 };
