b78ba28227f62fa6e6a5d500e371c26cb9ca1bbc
Vanessa Dugarte <dugartevanessa@gmail.com>
2024-05-02 20:19:00 -0400

    add Counter

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import "./itemListContainer.css"
+import ItemCount from "../itemCount/itemCount.jsx";
+import useCount from "../../hooks/useCount.js";
 const ItemListContainer = ({greeting}) => {
+    const {count, increment, decrement} = useCount(0);
     return (
         <div className="greeting">
           <h1>{greeting}</h1>
+            <ItemCount count={count} decrement={decrement} increment={increment} stock={5}/>
         </div>
     );
 };
