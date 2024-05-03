87abebd9c68c23184dce232a42079b0ca0b07edf
Vanessa Dugarte <dugartevanessa@gmail.com>
2024-05-02 22:10:46 -0400

    useEffect

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./itemListContainer.css"
 import useCount from "../../hooks/useCount.js";
 import Item from "../itemList/Item.jsx";
+import {getProducts} from "../../mock/asynMock.js";
 const ItemListContainer = ({greeting}) => {
+
+const [product, setProducts] = useState([]);
+const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        getProducts()
+            .then((data) => setProducts(data))
+            .finally(() => setIsLoading(false));
+    }, []);
 
     return (
         <div className="greeting">
           <h1>{greeting}</h1>
-        <Item item={{id: 3,
-            name: "Monstera Deliciosa",
-            price: 9990,
-            image: "https://media.admagazine.com/photos/61f0e1a0089751617cd2fe3e/1:1/w_1919,h_1919,c_limit/bugambilia.jpg",
-            description: "También llamada Costilla de Adán, es una planta tropical conocida por sus grandes hojas perforadas y su fácil cuidado. Es una opción popular para decorar interiores gracias a su exuberante follaje",
-            stock: 7}}/>
         </div>
     );
 };
